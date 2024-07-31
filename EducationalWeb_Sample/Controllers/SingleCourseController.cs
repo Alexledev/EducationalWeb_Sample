e7e8dc85f71ec086d6228b1137db09e15a4c0780
Alexe <lehongnhatminh@outlook.com>
2024-07-31 09:41:34 +0700

    Add project files.

---
+++ EducationalWeb_Sample/Controllers/SingleCourseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EducationalWeb_Sample.Controllers
+{
+    public class SingleCourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
